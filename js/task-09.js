398793948786ba5676652ee4242a69de1c72da74
Slavik-mvg <97396792+Slavik-mvg@users.noreply.github.com>
2024-01-12 13:15:26 +0100

    task 10

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+
+const colorText = document.querySelector('.color');
+console.dir(colorText.textContent);
+
+const button = document.querySelector(
+  'button.change-color'
+);
+
+// button.onclick = function (evt) {
+button.addEventListener('click', () => {
+  // console.log(evt);
+
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorText.textContent = `${getRandomHexColor()}`;
+});
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,63 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxesDiv = document.querySelector('#boxes');
+console.log(boxesDiv);
+
+let amount = 0;
+
+const btInput = document.querySelector('input');
+btInput.addEventListener('input', evt => {
+  // console.dir(evt.currentTarget.value);
+  amount = evt.currentTarget.value;
+});
+
+const btCreate = document.querySelector(
+  'button[data-create]'
+);
+// console.dir(btCreate);
+btCreate.addEventListener('click', () => {
+  // console.log(evt);
+  console.dir('jjkj');
+  createBoxes(amount);
+});
+
+let width = 20;
+let height = 20;
+
+//  console.log(amount);
+function createBoxes() {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${(width += 10)}px`;
+    box.style.height = `${(height += 10)}px`;
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+    boxesDiv.appendChild(box);
+    // boxesDiv.insertAdjacentElement('beforeend', box)
+
+    console.log(box);
+
+    console.log(boxesDiv);
+  }
+}
+
+const btDest = document.querySelector(
+  'button[data-destroy]'
+);
+console.dir(btDest);
+btDest.addEventListener('click', evt => {
+  console.dir('3333');
+
+  destroyBoxes();
+});
+
+function destroyBoxes() {
+  width = 20;
+  height = 20;
+  boxesDiv.innerHTML = [];
+  btInput.value = 0;
+}
+
+console.log(amount);
+// console.log(box);
